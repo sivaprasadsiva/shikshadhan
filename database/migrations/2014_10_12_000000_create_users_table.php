8482045f437f77328a9534a65fbe8fb436b8b027
sivaprasadsiva <sivaprasadsivank@gmail.com>
2024-02-15 10:51:16 +0530

    commit

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('username');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->string('mobile_number')->nullable()->unique();
+            $table->rememberToken();
+            $table->timestamps('created_on');
+            $table->string('created_by');
+            $table->timestamps('updated_on');
+            $table->string('updated_by');
+            $table->string('deleted');
+            $table->string('is_active');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+    }
+};
